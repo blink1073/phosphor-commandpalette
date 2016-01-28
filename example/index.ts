8c223557dfd9fb275a5ae873add42ace6f37b971
A. Darian <git@darian.af>
2016-01-28 17:09:29 +0000

    initial commit

---
+++ example/index.ts
@@ -0,0 +1,17 @@
+/*-----------------------------------------------------------------------------
+| Copyright (c) 2014-2016, PhosphorJS Contributors
+|
+| Distributed under the terms of the BSD 3-Clause License.
+|
+| The full license is in the file LICENSE, distributed with this software.
+|----------------------------------------------------------------------------*/
+'use strict';
+
+import {
+  CommandPalette
+} from 'phosphor-commandpalette';
+
+
+window.onload = () => {
+  console.log(typeof CommandPalette);
+};
